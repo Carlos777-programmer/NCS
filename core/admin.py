c3b94c105ad97818990cd3e77f8cd64a8844c448
Carlos777-programmer <carlosmarques_trabalho@hotmail.com>
2026-08-11 22:27:46 -0300

    feat: add login video, base templates, core routes and configurations

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Cliente, Veiculo
+from .models import Cliente, Veiculo, Servico, OrdemServico
 
 admin.site.register(Cliente)
 admin.site.register(Veiculo)
+admin.site.register(Servico)
+admin.site.register(OrdemServico)
