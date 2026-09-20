d8c84931e50b22f1116a3964de03c6f93bc65016
Carlos777-programmer <carlosmarques_trabalho@hotmail.com>
2026-09-20 14:54:57 -0300

    fix(backend): configure CORS_ALLOWED_ORIGINS for Netlify frontend integration

--- estetica_automotiva/settings.py
+++ estetica_automotiva/settings.py
@@ -44,7 +44,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://ncsgarage.netlify.app",
+]
 
 ROOT_URLCONF = 'estetica_automotiva.urls'
 
